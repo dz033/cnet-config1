21a3b51ad9b2e1a5d35b600453806bf6fb1da9c6
dz033 <121326266+dz033@users.noreply.github.com>
2023-11-09 09:54:57 +0800

    outlining components of compass

---
+++ compass.py
@@ -0,0 +1,19 @@
+
+"""
+stuff to implement:
+
+feature functions 
+1) shortest path
+    returns 1 (boolean) if P is the shortest path, else 0
+2) Egress
+    returns egress of P
+3) Ingress
+    returns ingress of P
+4) Organizational feature function 
+    returns org owning ip prefix
+
+    
+Feature score function
+
+Compass algorithm
+"""
