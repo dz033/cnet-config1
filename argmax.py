2124d6b4fcd2000e9d9df636234d99fc3680bc2b
notepad-coder <148338711+notepad-coder@users.noreply.github.com>
2023-11-23 05:21:55 +0800

    Update argmax.py

--- argmax.py
+++ argmax.py
@@ -1,13 +1,22 @@
 def argmax(feature_score, R, Q):
-  max_score = float('-inf')
-  selected_path = None
-  selected_feature = None
-  
-  for path in R:
-    for feature_function in Q:
-      score = feature_score(path, feature_function)
-      if score > max_score: 
-        max_score = score    
-        selected_path = path
-        selected_feature = feature_function
-        return selected_feature, selected_path
+    # Initialize variables to keep track of the maximum score, selected path, and selected feature
+    max_score = float('-inf')
+    selected_path = None
+    selected_feature = None
+
+    # Loop through each path in the set of routing paths (R)
+    for path in R:
+        # Loop through each feature function in the set of feature functions (Q)
+        for feature_function in Q:
+            # Calculate the score for the current path and feature function
+            score = feature_score(path, feature_function)
+
+            # Check if the calculated score is greater than the current maximum score
+            if score > max_score:
+                # If so, update the maximum score, selected path, and selected feature
+                max_score = score
+                selected_path = path
+                selected_feature = feature_function
+
+    # Return the selected feature and path with the highest score
+    return selected_feature, selected_path
