c53bb7c5363c5e930a84821a6bf64e3be954856d
dz033 <121326266+dz033@users.noreply.github.com>
2023-11-29 09:31:36 +0800

    progress on compass and general oop upgrades

--- compass.py
+++ compass.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+from path import RoutingPath, FeatureValue
+from featurescore import feature_score
+from argmax import argmax
 
 """
 stuff to implement:
@@ -22,26 +27,42 @@ line 10-13: while loop to find max specs of the feature selected
 
 do you need this equivalent path finder? seems like you would rarely find an equivalent path in the real world    
 """ 
-def compass(R, q1, q2, q3, q4, k, t): 
+
+
+
+def compass(R, Q, V, k, t, feature_score, argmax): 
     #R is a set of routing paths
     #q1...ql is a set of feature functions
     #k is the limit on the number of specifications
     #t is the limit on the size of the specifications
-    Q = {q1, q2, q3, q4}
+    
     S = set() #specification set S
     L = set() #last computed specification L
     while len(S) < k:
-        q, v = argmax(feature_score())  #code this part
+        q, v = argmax(feature_score())  #if weight is traffic size, wehre do we get this data
         L.add(v)
         Q.remove(q)
+        for value in V:
+            if value[0] == q:
+                V.remove(q)
         for path in R:
-            if not PathMeetsSpecification(path, v):
+            if not path_meets_specification(path, v):
                 R.remove(path)
         if len(L) == t:
             S = S.add(L)
             break
         #if there is an equivalent path to L with an additional feature value, add it instead because it will be more explanatory with the same coverage
-        while True: #there exists v belonging to Uq, for which LU{v} == L... (path equivalent): 
+        #while True: #there exists v belonging to Uq, for which LU{v} == L... (path equivalent): 
+        for value in V:
+            if check_path_equivalence(L, L.add(value), R):
+                L = L.add(value)
+                if len(L) == t:
+                    S = S.add(L)
+                    break
+                Q.remove(value[0])
+        
+
+        for path in L:
             L.add(v)
             if len(L) == t:
                 S.add(L)
@@ -51,40 +72,97 @@ def compass(R, q1, q2, q3, q4, k, t):
 
     return S
 
-def Egress(p):
-    return p.P[1] #simply returns egress from the NetworkPath of the RoutingPath
-
-def Ingress(p):
-    return p.P[0] #same logic as egress
-
-def Organization(p):
-    return p.d.organization
-
-def Shortest_Path(p):
-    if p.P[2].isShortestPath(): #figure out how to do this
-        return 1
-    return 0
-
-def CreateFeatureList(p): #creates a list of feature values of p for easy comparison to path specifications
-    l = [] 
-    
-    #ordered alphabetically
-    l.append(Egress(p))
-    l.append(Ingress(p))
-    l.append(Organization(p))
-    l.append(Shortest_Path(p))
-    
-    return l
-
-def PathMeetsSpecification(p, S):
-    #for all feature values in S, the corresponding feature value in p is the same. 
-    l = CreateFeatureList(p)
-    for feature_value in S.paths:
-        if feature_value not in l:  #if p meets S,  then all feature values in S are in p. necessary and sufficient condition
+def check_path_equivalence(L, S, R): #path equivalence is a property of path specifications
+    for path in R:
+        if path_meets_specification(path, L) and not path_meets_specification(path, S):
+            return False
+        elif not path_meets_specification(path, L) and path_meets_specification(path, S):
             return False
     return True
 
-def PathMeetsSpecificationSet(p, SS):
-    for specification in SS.specifications:     # call path meets specification for each specification in the set
-        PathMeetsSpecification(p, specification)
+def path_meets_specification(p, S):
+    #for all feature values in S, the corresponding feature value in p is the same. 
+
+    
+    for feature_value in S:
+        if feature_value.getType() == 'I':
+            if not p.checkIngress(): #return false if ingress doesnt match
+                return False
+        elif feature_value.getType() == 'E':
+            if not p.checkEgress(): 
+                return False
+        elif feature_value.getType() == 'O':
+            if not p.checkOrganization(): 
+                return False
+        elif feature_value.getType() == 'SP': #use elif here because we may add more feature values
+            if not p.checkShortestPath(): 
+                return False
     return True
+
+def path_meets_specification_set(p, SS):
+    for specification in SS.specifications:     # call path meets specification for each specification in the set
+        path_meets_specification(p, specification)
+    return True
+
+def main():
+    flows = pd.read_csv("flow-10-egress_10000.csv")
+    #print(f"flows: {flows}")
+    nested_list = flows.values.tolist() #path, org, ip, 
+    #print(nested_list) 
+
+
+
+
+    #print(flowList)
+    def generate_paths():
+        l = []
+        for node in nested_list:
+            destination = node[1]
+            ingress = node[0][0]
+            egress = node[0][-1]
+            path = node[0]
+            sp = False
+            bandwidth = node[3]
+            if node[4] == 'TRUE':
+                sp = True
+            
+            P = RoutingPath(destination, ingress, egress, path, sp, bandwidth)
+            #print(str(P))
+            l.append(P)
+        return l
+
+    pathList = generate_paths()
+    
+
+
+    #CREATING A LIST OF ALL FEATURE VALUES:
+    #WE ARE GENERATING THIS OURSELVES, BUT IN A REAL SCENARIO THESE VALUES SHOULD BE FIXED BASED ON THE NETWORK
+    ingressList = []
+    egressList = []
+    organizationList = []
+    spList = [True, False]
+    for path in pathList:
+        if path.getIngress() not in ingressList:
+            ingressList.append(path.getIngress())
+        if path.getEgress() not in egressList:
+            egressList.append(path.getEgress())
+        if path.getOrganization() not in ingressList:
+            organizationList.append(path.getOrganization())
+    
+    featureValueDict = {
+        "ingress": ingressList,
+        "egress": egressList,
+        "organization": organizationList,
+        "shortest_path": spList
+    }
+
+    S = compass(pathList, #R
+            featureValueDict.keys(), #Q
+            featureValueDict.items(), #V (not in the original paper, but added for path equivalence logic)
+            4,  #k
+            len(featureValueDict.keys()), #t
+            feature_score, 
+            argmax)
+    print(S)
+
+main()
--- path.py
+++ path.py
@@ -3,24 +3,72 @@ d = network prefix (organization owning traffic)
 P = path
 
 """
-class NetworkPath:
-    def __init__(self, ingress, egress, path):
-        self.ingress = ingress
-        self.egress = egress
-        self.path = path
-        self.P = [ingress, egress, path] #this is the most important attribute and the one we will be passing into RoutingPath
+
+import pandas as pd
+import numpy as np
+
+"""nodes = pd.read_csv('nodes.csv')
+tempNodesList = nodes.to_string().split("\n")
+nodesList = []
+for node in tempNodesList[1:]:  #start from 1 to ignore the header
+    cleanNode = node.split()
+    print(cleanNode)
+    nodesList.append(cleanNode)
+
+print(nodesList)"""
+
+
 
 class Destination:
     def __init__(self, ip, organization):
         self.ip = ip
         self.organization = organization
 
+class FeatureValue:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+        #example: type is ingress(I). check feature value
+    
+    def getType(self):
+        return self.type
+
 class RoutingPath:
-
-    def __init__(self, d, P): # d is class Destination P is class NetworkPath
+    def __init__(self, d, ingress, egress, path, shortestPath, bandwidth):
         self.d = d
-        self.P = P
+        self.ingress = ingress
+        self.egress = egress
+        self.path = path
+        self.shortestPath = shortestPath #this should be a boolean
+        self.bandwidth = bandwidth
+        self.listFormat = [self.d, self.ingress, self.egress, self.path, self.shortestPath, self.bandwidth]
 
+    def getIngress(self):
+        return self.ingress
+    def getEgress(self):
+        return self.ingress
+    def getOrganization(self):
+        return self.d
+    def getSP(self):
+        return self.shortestPath
+    def getBandwidth(self):
+        return self.bandwidth
+
+    def __str__(self):
+        return str(self.listFormat)
+    def checkDestination(self, value):
+        return self.d == value
+    def checkIngress(self, value):
+        return self.ingress == value
+    def checkEgress(self, value):
+        return self.egress == value
+    def checkPath(self, value):
+        return self.path == value
+    def checkShortestPath(self, value):
+        return self.shortestPath == value
+    
+    
+    
 class PathSpecification:
     def __init__(self, paths): #input paths as a list
         self.paths = paths
@@ -29,3 +77,30 @@ class SpecificationSet:
     def __init__(self, specifications, k): #input specifications as a list
         self.max_size = k
         self.specifications = specifications
+
+flows = pd.read_csv("flow-10-egress_10000.csv")
+#print(f"flows: {flows}")
+nested_list = flows.values.tolist() #path, org, ip, 
+#print(nested_list) 
+
+
+
+
+#print(flowList)
+def generatePaths():
+    l = []
+    for node in nested_list:
+        destination = node[1]
+        ingress = node[0][0]
+        egress = node[0][-1]
+        path = node[0]
+        sp = False
+        if node[4] == 'TRUE':
+            sp = True
+        
+        P = str(RoutingPath(destination, ingress, egress, path, sp))
+        print(P)
+        l.append(P)
+    return l
+
+print(generatePaths())
